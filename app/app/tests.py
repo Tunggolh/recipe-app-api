93b552d997ef6a821f088d0c6c6fa05e15c4de4f
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-03-31 16:31:00 +0800

    Add another simple test

--- app/app/tests.py
+++ app/app/tests.py
@@ -14,3 +14,9 @@ class CalcTests(SimpleTestCase):
         res = calc.add(20, 5)
 
         self.assertEqual(res, 25)
+
+    def test_substract_numbers(self):
+        """Test ubstracting numbers."""
+        res = calc.substract(10, 15)
+
+        self.assertEqual(res, 5)
