d15535f4b19623873f4ebbf4b57307ba21428732
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-03-31 23:54:37 +0800

    Fix lint line too long error

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,5 +21,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api_schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='api_schema'), name='api-docs')
+    path('api/docs/',
+         SpectacularSwaggerView.as_view(url_name='api_schema'),
+         name='api-docs')
 ]
